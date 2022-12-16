71b568655be23a475159b5e07b9b42b22cd2c1ae
Divya <beingdivyadixit@gmail.com>
2022-12-16 18:07:00 +0530

    Welcome Prgram

---
+++ ConsoleAppLCDay3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppLCDay3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison");
+        }
+    }
+}
